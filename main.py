04b29b806a3595639367fff78e77cd0f19a5c86f
Mahdi <mahdi.bani.2003@gmail.com>
2022-02-09 14:42:32 +0400

    It checks for right letters in wrong places

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 with open("Words.txt","r",encoding="utf-8") as file:
     words = file.read()
-
-words = list(set(list(words.replace("\u200c","").split())))
-words = [word.lower() for word in words]
-
+words = list(set(list(words.lower().replace("\u200c","").split())))
 print("Welcome to Wordle-Cheat cheat. This is just for fun")
-
 print("What should it have ?")
 inc = list(set(list(input().lower().replace(" ",""))))
-
 print("What should it not have ?")
 exc = list(set(list(input().lower().replace(" ",""))))
+print("Enter right letters in right places. Enter 0 for empty places.")
+right = list(input().lower())
+print("Enter right letters in wrong places. Enter 0 for empty places.")
+wrong = list(input().lower())
 
-print("Enter the order. Enter 0 for empty places.")
-order = list(input().lower())
 
 def includes(word,inc):
     for char in inc:
         if char not in word:
             return False
     return True
-
 def excludes(word,exc):
     for char in exc:
         if char in word:
@@ -32,8 +28,17 @@ def word_check(word,places):
             if word[i]!=places[i]:
                 return False
     return True
+def wrong_place(word,places):
+    for i in range(len(places)):
+        if places[i]!="0":
+            if word[i]==places[i]:
+                return False
+    return True
 
-res = [word for word in words if (len(word)==5 and includes(word,inc) and excludes(word,exc) and word_check(word,order))]
+
+
+
+res = [word for word in words if (len(word)==5 and includes(word,inc) and excludes(word,exc) and word_check(word,right) and wrong_place(word,wrong))]
 
 print("These words can be the answer: \n")
 print(*[i+"\n" for i in res])
